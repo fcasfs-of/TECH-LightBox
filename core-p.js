2d315289d3c7d95a41a9c91833a9214185ac158b
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2025-12-05 09:36:19 -0300

    Update core-p.js

--- core-p.js
+++ core-p.js
@@ -1,101 +1,86 @@
-var text_thnv="Fechar";
-
-  document.getElementById('btnImage').addEventListener('click', function(){
-fsmodal_open({
-  id:"previ_box",
-  tiptext:text_thnv,
-  scroll_hide:"yes",
-  noclose:false,
-  include:{
-    player:"no"
-  },
-  content:{
-    title:"Logo 1",
-    type:"image",
-    context:"https://fcasfs-of.cloud-fs.net/info-profile/logo/cover_b.png",
-    size:"150",
-    description:""
-  }
-});
-  });
-
-  document.getElementById('btnText').addEventListener('click',  function(){
-  fsmodal_open({
-  id:"previ_box",
-  tiptext:text_thnv,
-  scroll_hide:"yes",
-  noclose:false,
-  include:{
-    player:"no"
-  },
-  content:{
-    title:"LightBox",
-    type:"",
-    context:"Lightbox é inovador que oferece uma experiência integrada para visualizar e interagir com textos, links, imagens, áudios e vídeos de forma intuitiva e moderna. <br/> Com uma interface limpa e responsiva, o aplicativo permite aos usuários explorar conteúdos multimídia de maneira eficiente, proporcionando uma navegação fluida e envolvente.",
-    description:"(Texto, Link, Vídeo, Imagem e Áudio)"
-  }
-});
-  });
-
-  document.getElementById('btnLink').addEventListener('click',  function(){
-  fsmodal_open({
-  id:"previ_box",
-  tiptext:text_thnv,
-  scroll_hide:"yes",
-  noclose:false,
-  include:{
-    player:"no"
-  },
-  content:{
-    title:"Site",
-    type:"link",
-    context:"https://fcasfs-of.cloud-fs.net/",
-    size:"250",
-    description:"Oficial"
-  }
-});
-  });
-
-  document.getElementById('btnAudio').addEventListener('click',  function(){
- fsmodal_open({
-  id:"previ_box",
-  tiptext:text_thnv,
-  scroll_hide:"yes",
-  noclose:false,
-  include:{
-    player:"yes"
-  },
-  content:{
-    title:"Audio 1",
-    type:"audio",
-    context:"https://fcasfs-of.cloud-fs.net/info-profile/Files/intro1.mp4",
-    poster:"https://fcasfs-of.cloud-fs.net/info-profile/Files/intro1.jpg",
-    size:"250",
-    description:""
-  }
-});
-  });
-
-  document.getElementById('btnVideo').addEventListener('click',  function(){
-  fsmodal_open({
-  id:"previ_box",
-  tiptext:text_thnv,
-  scroll_hide:"yes",
-  noclose:false,
-  include:{
-    player:"yes"
-  },
-  content:{
-    title:"Video 1",
-    type:"video",
-    context:"https://fcasfs-of.cloud-fs.net/info-profile/Files/intro1.mp4",
-    poster:"https://fcasfs-of.cloud-fs.net/info-profile/Files/intro1.jpg",
-    size:"250",
-    description:""
-  }
-}); 
+  // Função para alternar temas
+  const toggleThemeBtn = document.getElementById('theme-toggle');
+  toggleThemeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    const isDark = document.body.classList.contains('dark');
+    toggleThemeBtn.textContent = isDark ? 'Modo Claro' : 'Modo Escuro';
   });
 
 
- 
- 
+  const text_thnv = "Fechar";
+
+  // Função para abrir modal
+  function openModal(contentOptions) {
+    fsmodal_open({
+      id: "previ_box",
+      tiptext: text_thnv,
+      scroll_hide: "yes",
+      noclose: false,
+      include: { player: contentOptions.includePlayer },
+      content: {
+        title: contentOptions.title,
+        type: contentOptions.type,
+        context: contentOptions.context,
+        poster: contentOptions.poster,
+        size: contentOptions.size,
+        description: contentOptions.description,
+      }
+    });
+  }
+
+  document.getElementById('btnImage').addEventListener('click', () => {
+    openModal({
+      title: "Logo 1",
+      type: "image",
+      context: "https://fcasfs-of.cloud-fs.net/info-profile/logo/cover_b.png",
+      size: "150",
+      includePlayer: "no",
+      description: ""
+    });
+  });
+
+  document.getElementById('btnText').addEventListener('click', () => {
+    openModal({
+      title: "LightBox",
+      type: "",
+      context: "Lightbox é inovador que oferece uma experiência integrada para visualizar e interagir com textos, links, imagens, áudios e vídeos de forma intuitiva e moderna. <br/> Com uma interface limpa e responsiva, o aplicativo permite aos usuários explorar conteúdos multimídia de maneira eficiente, proporcionando uma navegação fluida e envolvente.",
+      size: "250",
+      includePlayer: "no",
+      description: "(Texto, Link, Vídeo, Imagem e Áudio)"
+    });
+  });
+
+  document.getElementById('btnLink').addEventListener('click', () => {
+    openModal({
+      title: "Site",
+      type: "link",
+      context: "https://lightbox.fcasfs-of.cloud-fs.net/",
+      size: "250",
+      includePlayer: "no",
+      description: "Oficial"
+    });
+  });
+
+  document.getElementById('btnAudio').addEventListener('click', () => {
+    openModal({
+      title: "Audio 1",
+      type: "audio",
+      context: "https://fcasfs-of.cloud-fs.net/info-profile/Files/intro1.mp4",
+      poster: "https://fcasfs-of.cloud-fs.net/info-profile/Files/intro1.jpg",
+      size: "250",
+      includePlayer: "yes",
+      description: ""
+    });
+  });
+
+  document.getElementById('btnVideo').addEventListener('click', () => {
+    openModal({
+      title: "Video 1",
+      type: "video",
+      context: "https://fcasfs-of.cloud-fs.net/info-profile/Files/intro1.mp4",
+      poster: "https://fcasfs-of.cloud-fs.net/info-profile/Files/intro1.jpg",
+      size: "250",
+      includePlayer: "yes",
+      description: ""
+    });
+  });
