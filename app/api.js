401ef8620c401120d93e457c320b19008d4fce4c
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-03-17 09:54:00 -0300

    Update api.js

--- app/api.js
+++ app/api.js
@@ -84,7 +84,7 @@ var close_fsmofla_str='<span class="bngd">  <div style="margin-left:4px;" class=
  var close_fsmofla_efestr=" eff";
   if(optiy.remove_efect && optiy.remove_efect==true){   close_fsmofla_efestr="";   }
 
- var momocsifipsl = document.getElementById(optiy.id);   if(momocsifipsl){     momocsifipsl.innerHTML = "";    if(optiy.content){       momocsifipsl.innerHTML = ' <div class="modal'+close_fsmofla_efestr+'" id="fs_modal_'+optiy.id+'" style="display:block;color:#000;">  '+close_fsmofla_str+'       <div style="overflow:auto;" class="modal-content" id="fs_modal_'+optiy.id+'_content">'+fsmodal_create(optiy.content, optincludeplayer)+'</div><br/><br/></div>';    
+ var momocsifipsl = document.getElementById(optiy.id);   if(momocsifipsl){     momocsifipsl.innerHTML = "";    if(optiy.content){       momocsifipsl.innerHTML = ' <div class="modal'+close_fsmofla_efestr+'" id="fs_modal_'+optiy.id+'" style="display:block;color:#000;">  '+close_fsmofla_str+'       <div style="overflow:auto;" class="modal-content" id="fs_modal_'+optiy.id+'_content">'+fsmodal_create(optiy.content, optincludeplayer)+'</div><br/><br/></div>  <div id="cf_menuu"></div>   ';    
 
   var mffomocsifipsl = document.getElementById("fs_modal_"+optiy.id); 
  if(optiy.click_close && optiy.click_close==true){    mffomocsifipsl.onclick=function(){fsmodal_close(optiy.id, optiy.scroll_hide);};   }
@@ -94,7 +94,7 @@ function loaded_playerinf(dstart_fs_mpl){   if(typeof fs_Playerjs=="function" &&
 if(optincludeplayer && optincludeplayer=="yes" && is_player0j && is_player0j=="yes"){   loaded_playerinf(start_fs_mpl);   }
 
  if(close_fsmofla_strdd==true){    if(CtxMenu){
-  var MainContextMenu = CtxMenu();
+  var MainContextMenu = CtxMenu("cf_menuu");
     if(MainContextMenu){ 
     MainContextMenu.addItem("", function(){
      fsmodal_close(optiy.id, optiy.scroll_hide);
