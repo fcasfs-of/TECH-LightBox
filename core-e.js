a954210cef1ea1c94d2574283c719e00b6c5e2cd
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-08-27 09:02:01 -0300

    Update core-e.js

--- core-e.js
+++ core-e.js
@@ -8,7 +8,7 @@ let currentTheme = localStorage.getItem("LightBox_theme") || "light";
     const isDark = document.body.classList.contains('dark');
     toggleThemeBtn.textContent = isDark ? 'Light Mode' : 'Dark Mode';
     
-    localStorage.setItem("LightBox_theme", isDark === "dark" ? "light" : "dark";);
+    localStorage.setItem("LightBox_theme", isDark === "dark" ? "light" : "dark");
   });
 
 
