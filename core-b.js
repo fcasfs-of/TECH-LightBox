842106726db016cc6d5663e8635c1fc9d2428e32
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-08-28 09:58:06 -0300

    Create core-b.js

---
+++ core-b.js
@@ -0,0 +1,16 @@
+ var minhasOpcoes = [
+    {
+      key: "menu",
+      titulo: { pt: "Menu de Navegação", en: "Navigation Menu" },
+      padrao: true,
+      svg: '<svg viewBox="0 0 1024 1024"><path d="M160 448a32 32 0 01-32-32V160.064a32 32 0 0132-32h256a32 32 0 0132 32V416a32 32 0 01-32 32H160zm448 0a32 32 0 01-32-32V160.064a32 32 0 0132-32h255.936a32 32 0 0132 32V416a32 32 0 01-32 32H608zM160 896a32 32 0 01-32-32V608a32 32 0 0132-32h256a32 32 0 0132 32v256a32 32 0 01-32 32H160zm448 0a32 32 0 01-32-32V608a32 32 0 0132-32h255.936a32 32 0 0132 32v256a32 32 0 01-32 32H608z"/></svg>',
+      callback: function(v) { }
+    },
+    {
+      key: "remove_efect",
+      titulo: { pt: "Remover Animação", en: "Remove Animation" },
+      padrao: false,
+      svg: '<svg viewBox="0 0 24 24"><path d="M22,11H12a1,1,0,0,0-1,1V22a1,1,0,0,0,1,1H22a1,1,0,0,0,1-1V12A1,1,0,0,0,22,11ZM21,21H13V13h8ZM2,13a1,1,0,0,1-1-1V2A1,1,0,0,1,2,1H12a1,1,0,0,1,0,2H3v9A1,1,0,0,1,2,13ZM17,6a1,1,0,0,1,0,2H8v9a1,1,0,0,1-2,0V7A1,1,0,0,1,7,6Z"/></svg>',
+      callback: function(v) { }
+    }
+  ];
