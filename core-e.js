45b5998c2025eedda5ca9e5737eb9cc94b2d2216
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-08-28 22:03:56 -0300

    Update core-e.js

--- core-e.js
+++ core-e.js
@@ -93,10 +93,21 @@ function fcasfs_obter_booleano(chave, padrao){var v=fcasfs_lightbox_config_carre
     openModal({
       title: "LightBox",
       type: "",
-      context: "Lightbox is an innovative app that offers an integrated experience for viewing and interacting with text, list, graphic, links, Table, SVG, images, audio, and videos in an intuitive and modern way. <br/> With a clean and responsive interface, the app allows users to explore multimedia content efficiently, providing fluid and engaging navigation.",
+      context: "Lightbox is an innovative app that offers an integrated experience for viewing and interacting with text, carousel, list, graphic, links, Table, SVG, images, audio, and videos in an intuitive and modern way. <br/> With a clean and responsive interface, the app allows users to explore multimedia content efficiently, providing fluid and engaging navigation.",
       size: "250",
       includePlayer: "no",
-      description: "(Text, Link, SVG, Graphic, Table, List, Video, Image and Audio)"
+      description: "(Text, Link, SVG, Carousel, Graphic, Table, List, Video, Image and Audio)"
+    },e);
+  });
+
+  document.getElementById('btnList').addEventListener('click', (e) => {
+    openModal({
+      title: "Carousel",
+      type: "carrossel",
+      context:[ { url: "https://lightbox.fcasfs-of.cloud-fs.net/favicon.png", legenda: "" },  { url: "https://fcasfs-of.cloud-fs.net/info-profile/logo/cover_b.png", legenda: "" } ],
+      size: "250",
+      includePlayer: "no",
+      description: ""
     },e);
   });
 
