38d3d589df069c9a03e50f3726e4303f61db721f
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-08-28 22:05:18 -0300

    Update core-e.js

--- core-e.js
+++ core-e.js
@@ -100,7 +100,7 @@ function fcasfs_obter_booleano(chave, padrao){var v=fcasfs_lightbox_config_carre
     },e);
   });
 
-  document.getElementById('btnList').addEventListener('click', (e) => {
+  document.getElementById('btnCarrossel').addEventListener('click', (e) => {
     openModal({
       title: "Carousel",
       type: "carrossel",
