144ebfeab95a0cadc44c1b31baa6af93eec82a97
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-08-31 11:24:58 -0300

    Update api.js

--- app/api.js
+++ app/api.js
@@ -247,7 +247,10 @@ function fsmodal_create(optiy, app) {
             } else if (optiy.type && optiy.type == "image") {
                 is_player0j = "no";
                 fsmodal_create_ifir = `<img src="${optiy.context}" style="user-select:none;pointer-events:none; width:${Number(optiy.size)}px;" />`;
-            } else if (optiy.type && optiy.type == "svg") {
+            } else if (optiy.type && optiy.type == "texto") {
+                is_player0j = "no";
+                fsmodal_create_ifir = `<br/> <span class="txt"> ${optiy.context} </span>  <br/>   <style> .tlightboc_fcasfs .txt {   font-size: ${fcas_lightbox_checkValueEX(optiy,"config","fontSize","14px")};    color: ${fcas_lightbox_checkValueEX(optiy,"config","color","#fff")};  }   .tlightboc_fcasfs .txt hr {   border-color: ${fcas_lightbox_checkValueEX(optiy,"config","color","#fff")}; color: ${fcas_lightbox_checkValueEX(optiy,"config","color","#fff")};  } </style> <br/><br/>`;
+            }  if (optiy.type && optiy.type == "svg") {
                 is_player0j = "no";
                 fsmodal_create_ifir = `<br/> ${optiy.context}    <style> .tlightboc_fcasfs svg {   pointer-events: none;    width: ${Number(optiy.size)}px;    height: ${Number(optiy.size)}px;    fill: ${fcas_lightbox_checkValueEX(optiy,"config","fill","currentColor")};    stroke: ${fcas_lightbox_checkValueEX(optiy,"config","stroke","currentColor")};  }  </style> <br/><br/>`;
             }  else if (optiy.type && optiy.type == "audio") {
