97fb5985b6b2ef964e494098a1eeec9031f7d195
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-09-01 10:22:46 -0300

    Update api.js

--- app/api.js
+++ app/api.js
@@ -279,10 +279,10 @@ function fsmodal_create(optiy, app) {
                 start_fs_mpl = {
                     OSD: fcas_lightbox_checkValueEX(optiy,"config","OSD",false),
                     config: {
-						 osd:{  theme:fcas_lightbox_checkValueEX(optiy,"config","OSD_Theme","dark"), duration:3e3, width:"auto" },
-						volume:fcas_lightbox_checkValueEX(optiy,"config","volume","1"),
-						pos_time:fcas_lightbox_checkValueEX(optiy,"config","pos_time","0")
-					},
+                       osd:{  theme:fcas_lightbox_checkValueEX(optiy,"config","OSD_Theme","dark"), duration:3e3, width:"auto" },
+                       volume:fcas_lightbox_checkValueEX(optiy,"config","volume","1"),
+                       pos_time:fcas_lightbox_checkValueEX(optiy,"config","pos_time","0")
+                    },
                     id: "preview_pls",
                     customtext: { age: "" },
                     nocontrols: 0,
@@ -302,10 +302,10 @@ function fsmodal_create(optiy, app) {
                 start_fs_mpl = {
                     OSD: fcas_lightbox_checkValueEX(optiy,"config","OSD",false),
                     config: {
-						 osd:{  theme:fcas_lightbox_checkValueEX(optiy,"config","OSD_Theme","dark"), duration:3e3, width:"auto" },
-						volume:fcas_lightbox_checkValueEX(optiy,"config","volume","1"),
-						pos_time:fcas_lightbox_checkValueEX(optiy,"config","pos_time","0")
-					},
+                       osd:{  theme:fcas_lightbox_checkValueEX(optiy,"config","OSD_Theme","dark"), duration:3e3, width:"auto" },
+                       volume:fcas_lightbox_checkValueEX(optiy,"config","volume","1"),
+                       pos_time:fcas_lightbox_checkValueEX(optiy,"config","pos_time","0")
+                    },
                     id: "preview_pls",
                     customtext: { age: "" },
                     nocontrols: 0,
