43822db6f31f4504956573d8c07fc53b04e0877c
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-09-01 09:20:39 -0300

    Update api.js

--- app/api.js
+++ app/api.js
@@ -326,7 +326,11 @@ function fsmodal_create(optiy, app) {
 function loaded_playerinf(dstart_fs_mpl = {}) {
   if (typeof fs_Playerjs === "function") {
        mfplayeri = fs_Playerjs(dstart_fs_mpl);
-	  if(mfplayeri){   fplayeri.api('seek',Number(stringno_valtext(dstart_fs_mpl.config.pos_time,"0")));    fplayeri.api("volume", Number(stringno_valtext(dstart_fs_mpl.config.volume,"1")));  if(stringno_valtext(dstart_fs_mpl.config.volume,"")==""){   fplayeri.api("volume", 1);    }}
+	  if(mfplayeri){  
+		  playerhj.OnEvents("init",function(){  
+			  fplayeri.api('seek',Number(stringno_valtext(dstart_fs_mpl.config.pos_time,"0")));    
+			  fplayeri.api("volume", Number(stringno_valtext(dstart_fs_mpl.config.volume,"1")));  if(stringno_valtext(dstart_fs_mpl.config.volume,"")==""){   fplayeri.api("volume", 1);    }
+		});
    }
 }
 
