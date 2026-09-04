ac403c51b1cd958b0545835b9e6dbba904fcd14f
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-09-04 11:17:53 -0300

    Update core-e.js

--- core-e.js
+++ core-e.js
@@ -38,6 +38,7 @@ function fcasfs_obter_booleano(chave, padrao){var v=fcasfs_lightbox_config_carre
       itens: obj,      
       include: { player: contentOptions.includePlayer },
       content: {
+        config: {  scale:true },
         title: contentOptions.title,
         type: contentOptions.type,
         context: contentOptions.context,
