92718d11b13bfa9bd85efd914f5d8f26fad01aba
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-09-11 11:07:50 -0300

    Update docs_data.js

--- docs_data.js
+++ docs_data.js
@@ -52,7 +52,7 @@ window.getDocData = function() {
                 ['Configuração para tipo: "slideshow"', 'Suporta as chaves de estilização direta: effect ("slide", "fade", "zoom", "cube"), showControls (boolean), showCaptions (boolean).'],
                 ['Configuração para tipos: "audio", "video" e "PList"', 'Suporta a parametrização completa de players de mídia: colorText, colorIcon, OSD_Pos, pos_time (strings), OSD_Lang ("pt"/"en"), OSD_Theme ("light"/"dark"), volume (número decimal), OSD_Time (número em ms), OSD, autoplay, mute, contextmenu, nocontrols (booleanos). Se OSD for true, suporta a propriedade OSD_Events (opcional) que aceita uma lista/array contendo eventos como: "exitfullscreen", "fullscreen", "end", "finish", "volume", "seek", "userseek", "unmute", "mute", "pause", "stop", "play", "speed", "previous", "next".'],
                 ['Configuração Exclusiva para tipo: "PList"', 'Suporta adicionalmente a propriedade: select (string contendo apenas o número do item ativo da lista de reprodução).'],
-                ['Configuração para tipo: "form"', 'Suporta o controle de validação e envio: action (string), submitText (string), onsubmit (string).']
+                ['Configuração para tipo: "form"', 'Suporta o controle de validação e envio: method: (GET, POST, PUT, DELETE, PATCH), action (string), submitText (string), onsubmit (string).']
             ],
             context: [
                 ['Carrossel', 'Recebe um array de objetos contendo as chaves de caminhos e descrições: [ { url: "", legenda: "" } ]'],
@@ -130,7 +130,7 @@ window.getDocData = function() {
                 ['Configuration for type: "slideshow"', 'Properties: effect ("slide", "fade", "zoom", "cube"), showControls (boolean), showCaptions (boolean).'],
                 ['Configuration for types: "audio", "video" and "PList"', 'Properties: colorText, colorIcon, OSD_Pos, pos_time (strings), OSD_Lang ("pt"/"en"), OSD_Theme ("light"/"dark"), volume (float), OSD_Time (number in ms), OSD, autoplay, mute, contextmenu, nocontrols (booleans). If OSD is true, it supports the OSD_Events (optional) property which maps an array collection tracking media events like: "exitfullscreen", "fullscreen", "end", "finish", "volume", "seek", "userseek", "unmute", "mute", "pause", "stop", "play", "speed", "previous", "next".'],
                 ['Exclusive Configuration for type: "PList"', 'Properties: select (string tracking sequential track execution option array index number).'],
-                ['Configuration for type: "form"', 'Supports validation and submission control: action (string), submitText (string), onsubmit (string).']
+                ['Configuration for type: "form"', 'Supports validation and submission control: method: (GET, POST, PUT, DELETE, PATCH), action (string), submitText (string), onsubmit (string).']
             ],
             context: [
                 ['Carousel', 'Accepts an array of objects carrying media paths and string text captions: [ { url: "", legenda: "" } ]'],
