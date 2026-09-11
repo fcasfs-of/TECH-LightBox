effb487d4311bc79f638cc9bc270dbcce568f2be
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-09-11 08:56:28 -0300

    Update api.js

--- app/api.js
+++ app/api.js
@@ -626,14 +626,13 @@ document.getElementsByTagName("body")[0].appendChild(scrcontedfddd);
 var momocsifipsl = document.getElementById(optiy.id);
 if (momocsifipsl) {
 momocsifipsl.innerHTML='';
-function isadl_noeadult(){    fsmodal_close(optiy.id, optiy.scroll_hide);  }
-function isadl_yeseadult(){   optiy.isAdult=false;   fsmodal_open(optiy);  isadl_noeadult();   }
+function isadl_yeseadult(){   optiy.isAdult=false;   fsmodal_open(optiy);    }
 
 var fcaslightconetxndif=fsmodal_create(momocsifipsl_chd, optiy.content, optiy.include ? optiy.include.player : "");
 //var fcaslightconetxndiflink=fcas_lightbox_gerarTexto([fcas_lightbox_extrairTextoObjeto(optiy)]);
 
 var fcaslightconetxndif_isnort=fcaslightconetxndif;
-if (optiy.isAdult && optiy.isAdult===true){   fcaslightconetxndif_isnort=fcafs_lightbox_gerarAvisoConteudoAdulto({isAdult:optiy.isAdult, lang: fcas_lightbox_checkValueEX(optiy,"config","Lang","en") || "en", onNo:"isadl_noeadult();", onYes:"isadl_yeseadult();"});   }
+if (optiy.isAdult && optiy.isAdult===true){   fcaslightconetxndif_isnort=fcafs_lightbox_gerarAvisoConteudoAdulto({isAdult:optiy.isAdult, lang: fcas_lightbox_checkValueEX(optiy.content,"config","Lang","en") || "en", onNo:`fsmodal_close('${optiy.id}'${fsmodal_open_closegi});`, onYes:`fsmodal_close('${optiy.id}'${fsmodal_open_closegi});  isadl_yeseadult();`});   }
 
 //var scrcontedd = document.createElement("div");
 momocsifipsl.innerHTML = `
