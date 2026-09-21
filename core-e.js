9e5acfaf6b6f03a49003a6656638658cd4e121d1
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-09-21 11:07:59 -0300

    Update core-e.js

--- core-e.js
+++ core-e.js
@@ -50,7 +50,7 @@ function fcasfs_obter_booleano(chave, padrao){var v=fcasfs_lightbox_config_carre
       include: { player: contentOptions.includePlayer },
       content: {
         config: {
-          Lang:"en", action: "https://fcasfs-of.cloud-fs.net/projects-en"
+          Lang:"en", action: "https://fcasfs-of.cloud-fs.net/projects-en", autoClose:true,showReset:true
         },
         title: contentOptions.title,
         type: contentOptions.type,
