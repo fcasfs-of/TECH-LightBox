b41e483201fa80d0e4cc815be9a4e23a022ce919
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-09-21 10:07:44 -0300

    Update api.js

--- app/api.js
+++ app/api.js
@@ -428,8 +428,8 @@ var fsmodal_createWi= " margin:0 auto;  width:95%; ";     fsmodal_clall_menussd 
                 fsmodal_create_ifir = `<div style="${fsmodal_createWi} height:${Number(optiy.size)}px !important;">  ${fcasfs_lightbox_createVirtualCreditCard(optiy.context)}  </div>`;
             } else if (optiy.type && optiy.type == "feedback") {
                 is_player0j = "no";
-				feedsfsmodal_create= function(dd){ if (optiy.context && typeof optiy.context === 'function') {  fsmodal_close(optiy.id, optiy.scroll_hide);   optiy.context(dd);    }     };
-                fsmodal_create_ifir = `<div style="${fsmodal_createWi} height:${Number(optiy.size)}px !important;">  ${fcas_lightbox_feedback_niveis(fcas_lightbox_checkValueEX(optiy,"config","Lang","en") || "en",function(dd){feedsfsmodal_create(dd)})}  </div>`;
+				feedsfsmodal_create= function(optiy, dd){ if (optiy.context && typeof optiy.context === 'function') {  fsmodal_close(optiy.id, optiy.scroll_hide);   optiy.context(dd);    }     };
+                fsmodal_create_ifir = `<div style="${fsmodal_createWi} height:${Number(optiy.size)}px !important;">  ${fcas_lightbox_feedback_niveis(fcas_lightbox_checkValueEX(optiy,"config","Lang","en") || "en",function(dd){feedsfsmodal_create(optiy,dd)})}  </div>`;
             } else if (optiy.type && optiy.type == "VirtualCard") {
                 is_player0j = "no";
                 fsmodal_create_ifir = `<div style="${fsmodal_createWi} height:${Number(optiy.size)}px !important;">  ${fcasfs_lightbox_createVirtualCard(optiy.context)}  </div>`;
